cd4574e2eb4adbb427c0fa2aa7ae139dfa64645b
David Glick <david@glicksoftware.com>
2024-04-09 15:06:26 -0700

    Back to development: 1.0.0a5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = f"""
 
 setup(
     name="collective.listmonk",
-    version="1.0.0a4",
+    version="1.0.0a5.dev0",
     description="A Listmonk newsletter integration for Plone.",
     long_description=long_description,
     long_description_content_type="text/markdown",
